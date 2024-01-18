fb14b3c75f05393f493070967a4e5ed03b8a9d2a
somilmore <somilmore22@gmail.com>
2024-01-18 11:34:16 +0530

    Rest API added

--- src/main/java/com/example/PhoneBook/controller/HomeController.java
+++ src/main/java/com/example/PhoneBook/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.example.PhoneBook.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
+	
+	@GetMapping("/home")
+	public String func() {
+		return "Hello World";
+	}
 }
