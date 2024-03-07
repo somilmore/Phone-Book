b3106e1c94d1bda1bc62adf3cee4757d2af340e2
Somil More <117371840+somilmore@users.noreply.github.com>
2024-03-07 10:01:45 +0530

    Update HomeController.java

--- src/main/java/com/example/PhoneBook/controller/HomeController.java
+++ src/main/java/com/example/PhoneBook/controller/HomeController.java
@@ -38,7 +38,7 @@ public class HomeController {
 		List<Directory> list = repository.findAll();
 		
 		if(list.size()==0)
-			return "Hello World Kyzer devops!!";
+			return "Database is empty!";
 			
 		List<JSONObject> ans = service.getList(list);
 		
